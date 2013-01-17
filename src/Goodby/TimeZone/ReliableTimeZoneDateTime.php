35693ed8ba956aeca5b515ab8bd64e7389274cab
Mori Reo <mori.reo@gmail.com>
2013-01-17 16:34:35 +0900

    second timezone argument optional

--- src/Goodby/TimeZone/ReliableTimeZoneDateTime.php
+++ src/Goodby/TimeZone/ReliableTimeZoneDateTime.php
@@ -12,9 +12,18 @@ class ReliableTimeZoneDateTime extends DateTime
 	{
 		throw new Exception('You can\'t call format(). Please call by formatWithTimezone()');
 	}
-	
-	public function formatWithTimezone($format, $timezone)
+
+    /**
+     * @param $format
+     * @param string $timezone optional if not passed this argument will use default timezone
+     * @return string
+     */
+    public function formatWithTimezone($format, $timezone = null)
 	{
+        if ($timezone === null) {
+            $timezone = date_default_timezone_get();
+        }
+
 		return (new DateTime)
 			->setTimestamp($this->getTimestamp())
 			->setTimezone(new DateTimeZone($timezone))
